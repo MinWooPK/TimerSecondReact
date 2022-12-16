ab1c956282a75c0ba900f97ccd3602d9dc21e158
MinWooPK <minwoowork91@gmail.com>
2022-12-16 23:36:46 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
+
+
+const App = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  //TOGGLE PARA PAUSE AND START
+  function toggle() {
+    setIsActive(!isActive);
+  }
+  //FUNCTION FOR RESET
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+
+      //RESET
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  },
+    //PREGUNTAR
+    [isActive, seconds]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default App
+
+
+// function App() {
+
+//   const [count, setCount] = useState(0)
+
+//   useEffect(() => {
+//     console.log("useEffect - Mounting (initial render)");
+//     const id = setInterval(() => {
+//       setCount(count+ 1);
+//     }, 1000);
+
+//     // Return a "cleanup function" which will run automatically
+//     // before the component is removed from the DOM
+//     return () => {                                               // <== ADD
+//       console.log("Cleanup - Component Unmounting");
+//       clearInterval(id);
+//     };
+//   }, [count])
+  
+//   return (
+    
+//     <div>
+
+//       <h1> Timer </h1>
+
+//       <h2>{count}</h2>
+
+//     </div>
+
+// )
+
+// }
+// export default App;
+
+
+
+// function TimerThree() {
+//   const [count, setCount] = useState(0);
+
+
+//   useEffect(() => {
+//     console.log("useEffect - Mounting (initial render)");
+//     const id = setInterval(() => {
+//       setCount((prevCount) => prevCount + 1);
+//     }, 1000);
+
+//     // Return a "cleanup function" which will run automatically
+//     // before the component is removed from the DOM
+//     return () => {                                               // <== ADD
+//       console.log("Cleanup - Component Unmounting");
+//       clearInterval(id);
+//     };
+//   }, [])
+// }
